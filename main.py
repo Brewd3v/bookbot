7e36b7fa9bd0101859b099bd722542c2178483b3
James Marx <jmarx@haproxy.com>
2024-06-26 21:05:21 +0100

    char_count func

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+
+def count_words(contents):
+    words = contents.split()
+    return len(words)
+
+def char_count(contents):
+    char_map = {}
+    lower = contents.lower()
+
+    for char in lower:
+        print(char)
+        if char in char_map:
+            char_map[char] += 1
+        else:
+            char_map[char] = 1
+
+    return char_map
 
 main()
